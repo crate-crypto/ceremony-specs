a1b38af62c85b45d4078cdd90cd07921a3825e20
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2022-08-06 00:25:56 +0100

    - use SerialisedSRS instead of Ceremony - Change most of the terminology from `ceremony` to `srs`

--- sdk.py
+++ sdk.py
@@ -11,33 +11,31 @@ from common import hex_str
 # We are running four small ceremonies each of a different size
 NUM_OF_CEREMONIES = 4
 
-# All ceremonies require the same number of G2 points
-CEREMONY_NUM_G2_POWERS = 65
+# All srs's require the same number of G2 points
+SRS_NUM_G2_POWERS = 65
 
-CEREMONY_1_PARAMS = SRSParameters(4096, CEREMONY_NUM_G2_POWERS)  # 2^12
-CEREMONY_2_PARAMS = SRSParameters(8192, CEREMONY_NUM_G2_POWERS)  # 2^13
-CEREMONY_3_PARAMS = SRSParameters(16384, CEREMONY_NUM_G2_POWERS)  # 2^14
-CEREMONY_4_PARAMS = SRSParameters(32768, CEREMONY_NUM_G2_POWERS)  # 2^15
+SRS_1_PARAMS = SRSParameters(4096, SRS_NUM_G2_POWERS)  # 2^12
+SRS_2_PARAMS = SRSParameters(8192, SRS_NUM_G2_POWERS)  # 2^13
+SRS_3_PARAMS = SRSParameters(16384, SRS_NUM_G2_POWERS)  # 2^14
+SRS_4_PARAMS = SRSParameters(32768, SRS_NUM_G2_POWERS)  # 2^15
 
-TRANSCRIPT_PARAMS = [CEREMONY_1_PARAMS, CEREMONY_2_PARAMS,
-                     CEREMONY_3_PARAMS, CEREMONY_4_PARAMS]
-
-
-Ceremony = SerialisedSRS
+TRANSCRIPT_PARAMS = [SRS_1_PARAMS, SRS_2_PARAMS,
+                     SRS_3_PARAMS, SRS_4_PARAMS]
 
 
 @dataclass
 class Transcript:
     # We are running four small ceremonies in one.
-    # Python does not have typing like [Ceremony; 4]
+    # Python does not have typing like [SerialisedSRS; 4]
     # So we use a tuple with four elements. We could use
-    # Annotated[List[Ceremony], 4] , but since the number of ceremonies
+    # Annotated[List[SerialisedSRS], 4] , but since the number of ceremonies
     # will not change, this type hint coupled with said comment
     # is sufficient.
-    # The Ceremonies are assumed to be order in ascending order
+    # The SRS's are assumed to be order in ascending order
     # If this is not the case, when we check the NUM_G1_POWERS
     # It will fail
-    sub_ceremonies: Tuple[Ceremony, Ceremony, Ceremony, Ceremony]
+    sub_ceremonies: Tuple[SerialisedSRS,
+                          SerialisedSRS, SerialisedSRS, SerialisedSRS]
 
     def copy(self):
         from copy import deepcopy
@@ -49,19 +47,19 @@ class Transcript:
 def update_transcript(transcript: Transcript, secrets: List[hex_str]) -> Tuple[Transcript, UpdateProofs]:
     assert len(secrets) == NUM_OF_CEREMONIES
 
-    # Create a KeyPair for each ceremony using the provided secrets/randomness
+    # Create a KeyPair for each srs using the provided secrets/randomness
     keypairs: List[KeyPair] = []
     for secret in secrets:
 
         keypair = KeyPair(secret)
         keypairs.append(keypair)
 
-    # Emulate four Contributors, one for each ceremony
+    # Emulate four Contributors, one for each srs
     contributors: List[Contributor] = []
     for (keypair, ceremony, params) in zip(keypairs, transcript.sub_ceremonies, TRANSCRIPT_PARAMS):
 
-        assert ceremony.num_g1_powers == params.num_g1_points_needed
-        assert ceremony.num_g2_powers == params.num_g2_points_needed
+        assert ceremony.num_g1_points == params.num_g1_points_needed
+        assert ceremony.num_g2_points == params.num_g2_points_needed
 
         contributor = Contributor(keypair, params, ceremony)
         contributors.append(contributor)
@@ -79,10 +77,12 @@ def update_transcript(transcript: Transcript, secrets: List[hex_str]) -> Tuple[T
     #     if contributor.all_elements_in_correct_subgroup() == False:
     #         return None
 
-    # Update the old transcript in place
-    for (contributor, ceremony) in zip(contributors, transcript.sub_ceremonies):
-        ceremony.powers_of_tau = contributor.serialise_srs()
-    return (transcript, update_proofs)
+    # Create new transcript
+    list_of_srs = []
+    for contributor in contributors:
+        list_of_srs.append(contributor.serialise_srs())
+
+    return (Transcript(list_of_srs), update_proofs)
 
 
 def transcript_subgroup_check(transcript: Transcript) -> bool:
@@ -114,9 +114,9 @@ def verify_ceremonies(starting_transcript: Transcript, ending_transcript: Transc
     return True
 
 
-# This function assumes that the ceremony has been verified
-# Note: The position _should_ be the same for a contributor across ceremonies.
-# We could stop once we find the position in one ceremony, however this is an unnecessary optimisation
+# This function assumes that the srs has been verified
+# Note: The position _should_ be the same for a contributor across srs's.
+# We could stop once we find the position in one srs, however this is an unnecessary optimisation
 def find_contributions_no_verify(ceremony_update_proofs: List[UpdateProofs], pubkeys: List[PublicKey]) -> List[Optional[int]]:
     assert len(ceremony_update_proofs) == len(pubkeys)
     assert len(ceremony_update_proofs) == NUM_OF_CEREMONIES
