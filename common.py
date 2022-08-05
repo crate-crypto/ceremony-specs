3929e6ff112b902c0e77c7e6cbb008ad7237e6fa
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2022-08-05 20:13:34 +0100

    add hex_str as a type alias

--- common.py
+++ common.py
@@ -4,3 +4,10 @@ def pairwise(seq):
     window_size = 2
     for i in range(len(seq) - window_size + 1):
         yield (seq[i: i + window_size])
+
+
+# A hexadecimal string, without `0x` prepending it
+hex_str = str
+# TODO: This doesn't match the specs. The Json file has prepended 0x
+# TODO: Internally in the cryptography, we do not use 0x, though when we give it to the frontend
+# TODO we need to add it. The reason why the specs needs it is because of:https://ethereum.org/en/developers/docs/apis/json-rpc/#unformatted-data-encoding
